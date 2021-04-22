2fdf9e13833e0cc54903b01c5723381856037094
Клекчян Дмитрий <dima.klekchyan@gmail.com>
2021-04-22 10:29:21 +0500

    RecyclerView was added

--- AsteroidsNews/app/src/main/java/com/klekchyan/asteroidsnews/ListFragment.kt
+++ AsteroidsNews/app/src/main/java/com/klekchyan/asteroidsnews/ListFragment.kt
@@ -8,18 +8,24 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.klekchyan.asteroidsnews.databinding.FragmentListBinding
+import com.klekchyan.asteroidsnews.model.Asteroid
+import com.klekchyan.asteroidsnews.recycler.AsteroidsAdapter
 import com.klekchyan.asteroidsnews.utils.RequestType
 import com.klekchyan.asteroidsnews.utils.getListOfAsteroidsFromResponse
 import com.klekchyan.asteroidsnews.utils.getObserver
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
-import java.lang.StringBuilder
 
-class ListFragment : Fragment() {
+class ListFragment : Fragment(), AsteroidsAdapter.AsteroidsAdapterOnClickHandler {
 
     lateinit var binding: FragmentListBinding
+    lateinit var recyclerView: RecyclerView
+    lateinit var asteroidsAdapter: AsteroidsAdapter
+    lateinit var layoutManager: LinearLayoutManager
     var request: Disposable? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -28,33 +34,41 @@ class ListFragment : Fragment() {
                                             container,
                                             false)
 
+        layoutManager = LinearLayoutManager(activity)
+        asteroidsAdapter = AsteroidsAdapter(this)
+        recyclerView = binding.asteroidsRecyclerView
+
+        recyclerView.layoutManager = layoutManager
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = asteroidsAdapter
+
+        getAsteroids()
+
+        return binding.root
+    }
+
+    private fun getAsteroids(){
         val observer = getObserver(RequestType.ALL_ASTEROIDS)
-            .map { it }
+            .map { getListOfAsteroidsFromResponse(it) }
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
 
         request = observer.subscribe({
-            val asteroids = getListOfAsteroidsFromResponse(it)
-            var stringBuilder: StringBuilder = StringBuilder()
-            for(item in asteroids){
-                stringBuilder.append("${item.name} ${item.estimatedDiameter.meters.max} \n")
-            }
-            binding.listText.text = stringBuilder.toString()
+            asteroidsAdapter.setListOfAsteroids(it)
         }, {
             Log.e("Parsing", it.message.toString())
             Toast.makeText(context, "Exception was thrown", Toast.LENGTH_SHORT).show()
             //TODO Catch exceptions
         })
-
-        binding.testButton.setOnClickListener{
-            Toast.makeText(activity, "Button was clicked", Toast.LENGTH_SHORT).show()
-        }
-
-        return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         request?.dispose()
+    }
+
+    override fun onClickHandler(asteroid: Asteroid) {
+        Toast.makeText(activity, "Holder was clicked", Toast.LENGTH_SHORT).show()
+        //TODO Add navigation to next fragment
     }
 }
---
+++ AsteroidsNews/app/src/main/java/com/klekchyan/asteroidsnews/recycler/AsteroidsAdapter.kt
@@ -0,0 +1,74 @@
+package com.klekchyan.asteroidsnews.recycler
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.klekchyan.asteroidsnews.R
+import com.klekchyan.asteroidsnews.model.Asteroid
+import java.math.RoundingMode
+import java.text.DecimalFormat
+
+class AsteroidsAdapter(val listener: AsteroidsAdapterOnClickHandler): RecyclerView.Adapter<AsteroidsAdapter.AsteroidsViewHolder>() {
+
+    interface AsteroidsAdapterOnClickHandler{
+        fun onClickHandler(asteroid: Asteroid)
+    }
+
+    private var asteroids: MutableList<Asteroid> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AsteroidsViewHolder {
+
+        val context = parent.context
+        val layoutForItem = R.layout.asteroids_list_item
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(layoutForItem, parent, false)
+
+        return AsteroidsViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: AsteroidsViewHolder, position: Int) {
+        val asteroid: Asteroid = asteroids[position]
+
+        val df = DecimalFormat("#.###")
+        df.roundingMode = RoundingMode.CEILING
+
+        val sizeKilometers = "${df.format(asteroid.estimatedDiameter.kilometers.min)} - ${df.format(asteroid.estimatedDiameter.kilometers.max)} (km)"
+        val sizeMeters = "${df.format(asteroid.estimatedDiameter.meters.min)} - ${df.format(asteroid.estimatedDiameter.meters.max)} (m)"
+
+        holder.asteroidTextView.text = asteroid.name
+        holder.sizeKilometersText.text = sizeKilometers
+        holder.sizeMetersText.text = sizeMeters
+        holder.hazardousText.text = asteroid.isHazardous.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return if(asteroids.isEmpty()) 0 else asteroids.size - 1
+    }
+
+    fun setListOfAsteroids(asteroids: MutableList<Asteroid>){
+        this.asteroids = asteroids
+        notifyDataSetChanged()
+    }
+
+    inner class AsteroidsViewHolder(itemView: View,
+                              val asteroidTextView: TextView = itemView.findViewById(R.id.asteroidsNameText),
+                              val sizeKilometersText: TextView = itemView.findViewById(R.id.sizeKilometers),
+                              val sizeMetersText: TextView = itemView.findViewById(R.id.sizeMeters),
+                              val hazardousText: TextView = itemView.findViewById(R.id.isHazardous),
+                              val asteroidImage: ImageView = itemView.findViewById(R.id.asteroidImage)
+    ) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val asteroid = asteroids[bindingAdapterPosition]
+            listener.onClickHandler(asteroid)
+        }
+    }
+}
+
