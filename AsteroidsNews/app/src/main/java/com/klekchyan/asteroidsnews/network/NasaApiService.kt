027c2ff3c9207fe97d7126643c7e5ea659a98d90
Клекчян Дмитрий <dima.klekchyan@gmail.com>
2021-06-28 10:20:37 +0500

    Bug with SocketTimeoutException was fixed

--- AsteroidsNews/app/src/main/java/com/klekchyan/asteroidsnews/network/NasaApiService.kt
+++ AsteroidsNews/app/src/main/java/com/klekchyan/asteroidsnews/network/NasaApiService.kt
@@ -2,11 +2,13 @@ package com.klekchyan.asteroidsnews.network
 
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import kotlinx.coroutines.Deferred
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
+import java.util.concurrent.TimeUnit
 
 private const val NASA_API_BASE_URL = "https://api.nasa.gov/"
 private const val NASA_ASTEROIDS_NEOWS = "neo/rest/"
@@ -19,15 +21,21 @@ private const val PARAMETER_API_KEY = "api_key"
 
 private const val API_KEY = "iPIGH6W4IyUt01pS3cOtKOgLNYzTlFDoHMgY139m"
 
+private val client = OkHttpClient.Builder()
+        .connectTimeout(10, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .build()
+
 /*    NasaApi sends JSON with undefined item count,
 so it's needed to use ScalarsConverter to retrieve response as a String,
 and than it's needed to parse response on its own.
 To parse response this app has utils/ResponseParser. */
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
-    .addCallAdapterFactory(CoroutineCallAdapterFactory())
-    .baseUrl(NASA_API_BASE_URL + NASA_ASTEROIDS_NEOWS + NASA_ASTEROIDS_VERSION)
-    .build()
+        .client(client)
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .addCallAdapterFactory(CoroutineCallAdapterFactory())
+        .baseUrl(NASA_API_BASE_URL + NASA_ASTEROIDS_NEOWS + NASA_ASTEROIDS_VERSION)
+        .build()
 
 interface NasaApiService {
     @GET("feed")
